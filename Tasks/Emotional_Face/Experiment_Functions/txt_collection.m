0b22f3fb4882a9ead178ad8886a8601b40a66c30
tghafari <tara.ghafari@gmail.com>
2023-08-09 12:31:26 +0100

    fixed one sentence in txt_collection emoFace

--- Tasks/Emotional_Face/Experiment_Functions/txt_collection.m
+++ Tasks/Emotional_Face/Experiment_Functions/txt_collection.m
@@ -8,7 +8,7 @@ cfgTxt.question =  'Was the gender \n \n \n \n \n \n  female?                 ma
 cfgTxt.breakTxt = 'Take a break \n tell the experimenter to continue when you are ready';
 cfgTxt.endTxt = 'Thank you :-) \n\n Please stay still while we save your data.';
 instr1 = 'First a dot appears in the centre of the screen. \n\n Please look at that dot for the duration of the task.' ;
-instr2 = 'Then a face appears on the screen. \n\n Please continue looking at the dot.';
+instr2 = 'Then a face appears on the screen. \n\n Please continue to look at the dot.';
 instr3 = 'Sometimes a question will pop up \n asking about the gender of the previously shown face. \n\n Please press right index finger for male and left for female.';
 cfgTxt.instrTxt = {instr1, instr2, instr3};
 
