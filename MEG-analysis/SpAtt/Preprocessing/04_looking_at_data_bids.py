381fbf7c7431d5147235d26822cdb2f7948b70ff
tghafari <tara.ghafari@gmail.com>
2024-02-23 17:28:47 +0000

    updated the report

--- MEG-analysis/SpAtt/Preprocessing/04_looking_at_data_bids.py
+++ MEG-analysis/SpAtt/Preprocessing/04_looking_at_data_bids.py
@@ -55,7 +55,7 @@ raw.compute_psd(fmax=60).plot()
 mne.viz.plot_raw_psd(raw, fmin=.5, fmax=40, n_fft=None, picks=None)
 
 # Plot 10 first seconds of raw data
-raw.copy().crop(tmax=180).pick(["meg", "stim"]).filter(l_freq=0.1, h_freq=150).plot(title="raw")  # should be filtered bcz of cHPI high freq noise
+raw.copy().crop(tmax=180).pick(["meg", "stim"]).filter(l_freq=0.1, h_freq=80).plot(title="raw")  # should be filtered bcz of cHPI high freq noise
 
 # Raw file report with all chanels
 if summary_rprt:
@@ -64,11 +64,13 @@ if summary_rprt:
         os.makedirs(op.join(report_root , 'sub-' + subject, 'task-' + task))
     report_folder = op.join(report_root , 'sub-' + subject, 'task-' + task)
     report_fname = op.join(report_folder, 
-                            f'mneReport_sub-{subject}_{task}_2.hdf5')    # it is in .hdf5 for later adding images
-    html_report_fname = op.join(report_folder, f'report_preproc_{task}_2.html')
+                            f'mneReport_sub-{subject}_{task}_raw.hdf5')    # it is in .hdf5 for later adding images
+    html_report_fname = op.join(report_folder, f'report_preproc_{task}_raw.html')
 
-    raw.pick(["meg", "stim", "eog", "ecg"]).filter(l_freq=0.1, h_freq=150).load_data()
+    raw.pick(["meg", "stim", "eog", "ecg"]).filter(l_freq=0.1, h_freq=80).load_data()
 
     report = mne.Report(title=f'sub-{subject}_{task}')
-    report.add_raw(raw=raw, title='Raw', psd=True)
-    report.save(report_fname, overwrite=True, open_browser=True)
+    report.add_raw(raw=raw, title='Raw', psd=True,
+                   tags=('raw')) 
+    report.save(report_fname, overwrite=True)
+    report.save(html_report_fname, overwrite=True, open_browser=True)  # to check how the report looks
